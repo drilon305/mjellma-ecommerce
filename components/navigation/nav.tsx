09517c6df2e75e1bbefda08321af84e0cd3d32bb
Drilon <navycrack1@gmail.com>
2024-06-11 00:28:32 +0200

    styling login page

--- components/navigation/nav.tsx
+++ components/navigation/nav.tsx
@@ -8,7 +8,7 @@ export default async function Nav() {
   const session = await auth();
 
   return (
-    <header className="bg-red-400 py-4">
+    <header className="py-8">
       <nav>
         <ul className="flex justify-between">
           <li>Logo</li>
