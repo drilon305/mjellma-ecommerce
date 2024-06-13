20e88fa1bdd75f4ef1b5609f5ca0081d4085d75c
Drilon <navycrack1@gmail.com>
2024-06-13 20:14:57 +0200

    form error and success component

--- server/actions/email-signin.ts
+++ server/actions/email-signin.ts
@@ -2,11 +2,27 @@
 
 import { LoginSchema } from "@/types/login-schema";
 import { createSafeActionClient } from "next-safe-action";
+import { db } from "..";
+import { eq } from "drizzle-orm";
+import { users } from "../schema";
 
 export const action = createSafeActionClient();
 
 export const emailSignIn = action(LoginSchema, async ({email, password, code}) => {
+
+    const existingUser = await db.query.users.findFirst({
+        where: eq(users.email, email),
+    })
+
+
+    if(existingUser?.email !== email) {
+        return { error: 'Email not found' };
+    }
+
+
+
+
     console.log(email, password, code);
-    return { email }
+    return {success: email}
 }
 )
