ca8348f7eb1074888634be5b9af287699b54e5b7
Drilon <navycrack1@gmail.com>
2024-07-23 20:14:43 +0200

    creating datatable

--- app/dashboard/products/page.tsx
+++ app/dashboard/products/page.tsx
@@ -1,7 +1,25 @@
+import { db } from "@/server"
+import placeholder from '@/public/placeholder.svg'
+import { DataTable } from "./data-table"
+
+export default async function Products() {
+  const products = await db.query.products.findMany({
+    orderBy: (products, {desc}) => [desc(products.id)],
+  })
+  if(!products) throw new Error('No products found')
 
 
-export default function Products() {
+    const dataTable = products.map((product) => {
+      return {
+        id: product.id,
+        title: product.title,
+        price: product.price,
+        variants: [],
+        image: placeholder.src,
+      }
+    })
+
+    if(!dataTable) throw new Error('No data found')
   return (
-    <div>page</div>
-  )
+    <DataTable columns={} data={dataTable} />
 }
