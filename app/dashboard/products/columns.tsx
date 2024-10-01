c11aa24021044b68f469d66d78bcf66d9009ec58
Drilon <navycrack1@gmail.com>
2024-10-02 00:08:20 +0200

    edit and delete products

---
+++ app/dashboard/products/columns.tsx
@@ -0,0 +1,89 @@
+"use client"
+
+import { ColumnDef } from "@tanstack/react-table"
+import { MoreHorizontal } from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import Image from "next/image"
+
+
+type ProductColumn = {
+    title: string;
+    price: number;
+    image: string;
+    variants: any;
+    id: number;
+}
+
+export const columns: ColumnDef<ProductColumn>[] = [
+  {
+    accessorKey: "id",
+    header: "ID",
+  },
+  {
+    accessorKey: "title",
+    header: "Title",
+  },
+  {
+    accessorKey: "variants",
+    header: "Variants",
+  },
+  {
+    accessorKey: "price",
+    header: "Price",
+    cell: ({ row }) => {
+      const price = parseFloat(row.getValue("price"));
+      const formatted = new Intl.NumberFormat("en-US", {
+        currency: "EUR",
+        style: "currency",
+      }).format(price);
+      return <div className="font-medium text-xs">{formatted}</div>;
+    },
+  },
+  {
+    accessorKey: "image",
+    header: "Image",
+    cell: ({ row }) => {
+      const cellImage = row.getValue("image") as string;
+      const cellTitle = row.getValue("title") as string;
+      return (
+        <div className="">
+          <Image
+            src={cellImage}
+            alt={cellTitle}
+            width={50}
+            height={50}
+            className="rounded-md"
+          />
+        </div>
+      );
+    },
+  },
+  {
+    id: "actions",
+    header: "Actions",
+    cell: ({ row }) => {
+      const product = row.original;
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant={'ghost'} className="w-8 h-8 p-0">
+                <MoreHorizontal className="w-4 h-4" /></Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem className="dark:focus:bg-primary focus:bg-primary/50 cursor-pointer">Edit Product</DropdownMenuItem>
+            <DropdownMenuItem className="dark:focus:bg-destructive focus:bg-destructive/50 cursor-pointer">Delete Product</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
