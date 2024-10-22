f48ed2c55e92bc6d59b1edc39a1ce98a214a6333
Drilon <navycrack1@gmail.com>
2024-10-23 00:11:00 +0200

    variant form

--- app/dashboard/products/product-variant.tsx
+++ app/dashboard/products/product-variant.tsx
@@ -1,9 +1,123 @@
-'use client'
+"use client";
 
-export default function ProductVariant() {
-    return (
-        <div>
-        <h2>hey</h2>
-        </div>
-    )
+import { VariantsWithImagesTags } from "@/lib/infer-type";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+  } from "@/components/ui/dialog"
+  import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+  } from "@/components/ui/form"
+import { useForm } from "react-hook-form";
+import * as z from 'zod'
+import { zodResolver } from "@hookform/resolvers/zod";
+import { VariantSchema } from "@/types/variant-schema";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+  
+
+export default function ProductVariant({
+  editMode,
+  productID,
+  variant,
+  children,
+}: {
+  editMode: boolean;
+  productID: number;
+  variant?: VariantsWithImagesTags;
+  children: React.ReactNode;
+}) {
+
+    const form = useForm<z.infer<typeof VariantSchema>>({
+        resolver: zodResolver(VariantSchema),
+        defaultValues: {
+            tags: [],
+            variantImages: [],
+            color: '#000000',
+            editMode,
+            id: undefined,
+            productID,
+            productType: 'Black Notebooks'
+
+        },
+    })
+
+    function onSubmit(values: z.infer<typeof VariantSchema>) {
+        console.log(values)
+      }
+
+  return (
+    <Dialog>
+    <DialogTrigger>{children}</DialogTrigger>
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>{editMode ? 'Edit' : 'Create'} your variant</DialogTitle>
+        <DialogDescription>
+         Manage your product variants here.
+        </DialogDescription>
+      </DialogHeader>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+        <FormField
+          control={form.control}
+          name="productType"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Variant Title</FormLabel>
+              <FormControl>
+                <Input placeholder="Pick a title for your variant" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="color"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Variant Color</FormLabel>
+              <FormControl>
+                <Input type="color" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        {/* <FormField
+          control={form.control}
+          name="tags"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Variant Tags</FormLabel>
+              <FormControl>
+               <InputTags />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        /> */}
+        {editMode && variant && (
+            <Button type="button" onClick={(e) => e.preventDefault()}>
+                Delete Variant
+            </Button>
+        )}
+        {/* <VariantImages /> */}
+        <Button type="submit">{editMode ? 'Update Variant' : 'Create Variant'}</Button>
+      </form>
+    </Form>
+
+    </DialogContent>
+  </Dialog>
+  );
 }
